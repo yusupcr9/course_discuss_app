49cb608e02a465b7dc769493e49d90540ec5e272
Yusup Firmansyah <yhusup6@gmail.com>
2023-06-24 21:59:17 +0700

    Route Config

---
+++ lib/config/app_route.dart
@@ -0,0 +1,46 @@
+import 'package:course_discuss_app/config/session.dart';
+import 'package:course_discuss_app/page/error_page.dart';
+import 'package:course_discuss_app/page/login_page.dart';
+import 'package:go_router/go_router.dart';
+
+import '../model/user.dart';
+import '../page/home_page.dart';
+import '../page/register_page.dart';
+
+class AppRoute {
+  static const home = '/';
+  static const login = '/login';
+  static const register = '/register';
+
+  static GoRouter routerConfig = GoRouter(
+    errorBuilder: (context, state) => ErrorPage(
+      title: 'Something Error',
+      description: state.error.toString(),
+    ),
+    debugLogDiagnostics: true,
+    redirect: (context, state) async {
+      User? user = await Session.getUser();
+      if (user == null) {
+        if (state.location == login || state.location == register) {
+          return null;
+        }
+        return login;
+      }
+      return null;
+    },
+    routes: [
+      GoRoute(
+        path: home,
+        builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: login,
+        builder: (context, state) => const LoginPage(),
+      ),
+      GoRoute(
+        path: register,
+        builder: (context, state) => const RegisterPage(),
+      ),
+    ],
+  );
+}
