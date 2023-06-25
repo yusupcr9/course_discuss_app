48b629d7f1a78baaea2f122eb84466b519d09e34
Yusup Firmansyah <yhusup6@gmail.com>
2023-06-25 13:00:11 +0700

    complete service follow

--- lib/source/follow_source.dart
+++ lib/source/follow_source.dart
@@ -4,6 +4,8 @@ import 'package:course_discuss_app/config/api.dart';
 import 'package:d_method/d_method.dart';
 import 'package:http/http.dart';
 
+import '../model/user.dart';
+
 class FollowSource {
   static Future<bool> checkIsFollowing(
     String fromIdUser,
@@ -21,6 +23,92 @@ class FollowSource {
     } catch (e) {
       DMethod.printTitle("Follow Resource - checkIsFollowing", e.toString());
       return false;
+    }
+  }
+
+  static Future<bool> following(
+    String fromIdUser,
+    String toIdUser,
+  ) async {
+    String url = '${Api.follow}/following.php';
+    try {
+      Response response = await Client().post(Uri.parse(url), body: {
+        'from_id_user': fromIdUser,
+        'to_id_user': toIdUser,
+      });
+      DMethod.printTitle('Follow Resource - following', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody['success'];
+    } catch (e) {
+      DMethod.printTitle("Follow Resource - following", e.toString());
+      return false;
+    }
+  }
+
+  static Future<bool> noFollowing(
+    String fromIdUser,
+    String toIdUser,
+  ) async {
+    String url = '${Api.follow}/no_following.php';
+    try {
+      Response response = await Client().post(Uri.parse(url), body: {
+        'from_id_user': fromIdUser,
+        'to_id_user': toIdUser,
+      });
+      DMethod.printTitle('Follow Resource - noFollowing', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody['success'];
+    } catch (e) {
+      DMethod.printTitle("Follow Resource - noFollowing", e.toString());
+      return false;
+    }
+  }
+
+  static Future<List<User>> readFollower(
+    String idUser,
+  ) async {
+    String url = '${Api.follow}/read_follower.php';
+    try {
+      Response response = await Client().post(Uri.parse(url), body: {
+        'id_user': idUser,
+      });
+      DMethod.printTitle('Follow Resource - readFollower', response.body);
+      Map responseBody = jsonDecode(response.body);
+      if (responseBody['success']) {
+        List list = responseBody['data'];
+        return list.map((e) {
+          Map<String, dynamic> item = Map<String, dynamic>.from(e);
+          return User.fromJson(item);
+        }).toList();
+      }
+      return [];
+    } catch (e) {
+      DMethod.printTitle("Follow Resource - readFollower", e.toString());
+      return [];
+    }
+  }
+
+  static Future<List<User>> readFollowing(
+    String idUser,
+  ) async {
+    String url = '${Api.follow}/read_following.php';
+    try {
+      Response response = await Client().post(Uri.parse(url), body: {
+        'id_user': idUser,
+      });
+      DMethod.printTitle('Follow Resource - readFollowing', response.body);
+      Map responseBody = jsonDecode(response.body);
+      if (responseBody['success']) {
+        List list = responseBody['data'];
+        return list.map((e) {
+          Map<String, dynamic> item = Map<String, dynamic>.from(e);
+          return User.fromJson(item);
+        }).toList();
+      }
+      return [];
+    } catch (e) {
+      DMethod.printTitle("Follow Resource - readFollowing", e.toString());
+      return [];
     }
   }
 }
