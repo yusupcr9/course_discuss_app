9d9488e2b0cd5eb91134b9938f98bb1a770f9643
Yusup Firmansyah <yhusup6@gmail.com>
2023-07-01 16:23:26 +0700

    Complete Feed & Explore Fragment

--- lib/page/fragment/feed_fragment.dart
+++ lib/page/fragment/feed_fragment.dart
@@ -1,18 +1,60 @@
+import 'dart:convert';
+
 import 'package:course_discuss_app/config/session.dart';
+import 'package:course_discuss_app/model/topic.dart';
+import 'package:course_discuss_app/widget/item_topic.dart';
 import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../controller/c_feed.dart';
+import '../../controller/c_user.dart';
 
 class FeedFragment extends StatelessWidget {
   const FeedFragment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-          onPressed: () {
-            Session.clearUser();
-          },
-          child: Text("Logout")),
+    final cUser = context.read<CUser>();
+    if (cUser.data == null) return DView.loadingCircle();
+    context.read<CFeed>().setTopics(cUser.data!.id);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+          child: DView.textTitle('Feed', size: 24),
+        ),
+        Expanded(child: Consumer<CFeed>(builder: (contextConsumer, _, child) {
+          if (_.topics.isEmpty) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                DView.empty(),
+                DView.empty("Or you are not following anybody"),
+              ],
+            );
+          }
+          return ListView.builder(
+              itemCount: _.topics.length,
+              itemBuilder: (context, index) {
+                Topic topic = _.topics[index];
+                List images = jsonDecode(topic.images);
+                return Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    16,
+                    8,
+                    16,
+                    index == _.topics.length - 1 ? 30 : 8,
+                  ),
+                  child: ItemTopic(
+                    topic: topic,
+                    images: images,
+                  ),
+                );
+              });
+        }))
+      ],
     );
   }
 }
